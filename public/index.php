79d8c63eec1f1783228bcf98a4f9db0b480db515
Alexander Makarov <sam@rmcreative.ru>
2019-06-08 00:05:01 +0300

    Turn on rebuilding by default

--- public/index.php
+++ public/index.php
@@ -7,6 +7,9 @@ use yii\di\Container;
 (function () {
     require_once dirname(__DIR__) . '/vendor/autoload.php';
 
+    // Don't do it in production, assembling takes it's time
+    Builder::rebuild();
+
     $container = new Container(require Builder::path('web'));
 
     //Yii::setContainer($container);
