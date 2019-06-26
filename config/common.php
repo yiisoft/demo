a96cf332cc0c2900f157ce2de811e6aa00862292
Дмитрий Егоров <desure85@gmail.com>
2019-06-26 08:33:18 +0300

    \yii\di\Reference -> Yiisoft\Factory\Definitions\Reference (#2)

--- config/common.php
+++ config/common.php
@@ -1,22 +1,31 @@
 <?php
 
+use yii\base\Aliases;
+use yii\di\Container;
+use Yiisoft\Cache\ArrayCache;
+use Yiisoft\Cache\Cache;
+use Yiisoft\Db\Connection;
+use Yiisoft\Factory\Definitions\Reference;
+use Yiisoft\Log\FileRotator;
+use Yiisoft\Log\Logger;
+
 $params = $params ?? [];
 
 return [
     'array-cache' => [
-        '__class' => \Yiisoft\Cache\ArrayCache::class,
+        '__class' => ArrayCache::class,
     ],
     'cache' => [
-        '__class' => \Yiisoft\Cache\Cache::class,
+        '__class' => Cache::class,
         '__construct()' => [
-            0 => \yii\di\Reference::to('array-cache'),
+            0 => Reference::to('array-cache'),
         ],
         'handler' => [
-            '__class' => \Yiisoft\Cache\ArrayCache::class,
+            '__class' => ArrayCache::class,
         ],
     ],
     'db' => [
-        '__class'   => \Yiisoft\Db\Connection::class,
+        '__class'   => Connection::class,
         'dsn'       => 'sqlite:dbname=' . $params['db.name']
             . (!empty($params['db.host']) ? (';host=' . $params['db.host']) : '')
             . (!empty($params['db.port']) ? (';port=' . $params['db.port']) : ''),
@@ -24,18 +33,18 @@ return [
         'password'  => $params['db.password'],
     ],
     'file-rotator' => [
-        '__class' => \Yiisoft\Log\FileRotator::class,
+        '__class' => FileRotator::class,
         '__construct()' => [
             10
         ]
     ],
-    'logger' => static function (\yii\di\Container $container) {
-        /** @var \yii\base\Aliases $aliases */
+    'logger' => static function (Container $container) {
+        /** @var Aliases $aliases */
         $aliases = $container->get('aliases');
 
         $fileTarget = new Yiisoft\Log\FileTarget($aliases->get('@runtime/logs/app.log'),  $container->get('file-rotator'));
 
-        return new \Yiisoft\Log\Logger([
+        return new Logger([
             'file' => $fileTarget->setCategories(['application']),
         ]);
     },
