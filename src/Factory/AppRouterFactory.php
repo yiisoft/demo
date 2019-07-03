e72a59d28b7c961ab073323c5d8e6591b6ea3852
Alexander Makarov <sam@rmcreative.ru>
2019-07-03 17:02:29 +0300

    Rename Controller middleware to ActionCaller

--- src/Factory/AppRouterFactory.php
+++ src/Factory/AppRouterFactory.php
@@ -5,7 +5,7 @@ use Psr\Container\ContainerInterface;
 use Yiisoft\Router\FastRoute\FastRouteFactory;
 use Yiisoft\Router\Route;
 use Yiisoft\Router\RouterFactory;
-use Yiisoft\Yii\Web\Middleware\Controller;
+use Yiisoft\Yii\Web\Middleware\ActionCaller;
 use App\Controller\SiteController;
 
 class AppRouterFactory
@@ -13,8 +13,8 @@ class AppRouterFactory
     public function __invoke(ContainerInterface $container)
     {
         $routes = [
-            Route::get('/')->to(new Controller(SiteController::class, 'index', $container)),
-            Route::get('/test/{id:\w+}')->to(new Controller(SiteController::class, 'testParameter', $container))
+            Route::get('/')->to(new ActionCaller(SiteController::class, 'index', $container)),
+            Route::get('/test/{id:\w+}')->to(new ActionCaller(SiteController::class, 'testParameter', $container))
         ];
 
         return (new RouterFactory(new FastRouteFactory(), $routes))($container);
