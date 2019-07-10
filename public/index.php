8feed8a2064517f1a8105d65ee51724ad7d3b56c
Alexander Makarov <sam@rmcreative.ru>
2019-07-11 00:37:17 +0300

    Adjust to Aliases class extracted into yiisoft/aliases

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 use hiqdev\composer\config\Builder;
 use Yiisoft\Di\Container;
 
-//use yii\helpers\Yii;
-
 (function () {
     require_once dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -12,13 +10,10 @@ use Yiisoft\Di\Container;
 
     $container = new Container(require Builder::path('web'));
 
-    /* @var \yii\base\Aliases $aliases */
-    $aliases = $container->get(\yii\base\Aliases::class);
+    $aliases = $container->get(\Yiisoft\Aliases\Aliases::class);
     $aliases->set('@web', __DIR__);
     $aliases->set('@base', dirname(__DIR__));
     $aliases->set('@views', dirname(__DIR__) . '/views');
-
-    //Yii::setContainer($container);
 
     $container->get('app')->run();
 })();
