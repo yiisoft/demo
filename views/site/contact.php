5308abb7519bb86f929183187c433f1781243aa7
razonyang <razonyang@gmail.com>
2019-07-21 12:10:52 +0800

    Remove useless JS script

--- views/site/contact.php
+++ views/site/contact.php
@@ -34,4 +34,3 @@
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
 
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
