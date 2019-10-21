c2563c74df1e6e96062858adcb2aa0d5dba4e9fa
Alexander Makarov <sam@rmcreative.ru>
2019-10-21 11:56:35 +0300

    Remove unneeded imports

--- src/Factory/CycleDbalFactory.php
+++ src/Factory/CycleDbalFactory.php
@@ -2,27 +2,10 @@
 
 namespace App\Factory;
 
-use Cycle\Annotated;
-use Cycle\ORM\Factory;
-use Cycle\ORM\ORM;
-use Cycle\ORM\Schema;
-use Cycle\ORM\SchemaInterface;
-use Cycle\Schema\Compiler;
-use Cycle\Schema\Generator\GenerateRelations;
-use Cycle\Schema\Generator\GenerateTypecast;
-use Cycle\Schema\Generator\RenderRelations;
-use Cycle\Schema\Generator\RenderTables;
-use Cycle\Schema\Generator\ResetTables;
-use Cycle\Schema\Generator\SyncTables;
-use Cycle\Schema\Generator\ValidateEntities;
-use Cycle\Schema\Registry;
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use Psr\Container\ContainerInterface;
 use Spiral\Database\Config\DatabaseConfig;
 use Spiral\Database\DatabaseManager;
 use Spiral\Database\Driver\SQLite\SQLiteDriver;
-use Spiral\Tokenizer\ClassLocator;
-use Symfony\Component\Finder\Finder;
 use Yiisoft\Aliases\Aliases;
 
 class CycleDbalFactory
