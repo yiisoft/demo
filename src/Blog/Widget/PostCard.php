6cdd15ddd62060bbda49bab09acfbf9f499bc542
roxblnfk <roxblnfk@ya.ru>
2020-02-09 13:21:23 +0300

    added PostCard widget

---
+++ src/Blog/Widget/PostCard.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Blog\Widget;
+
+use App\Blog\Entity\Post;
+use Yiisoft\Router\UrlGeneratorInterface;
+use Yiisoft\Yii\Bootstrap4\Html;
+use Yiisoft\Yii\Bootstrap4\Widget;
+
+class PostCard extends Widget
+{
+    private ?Post $post = null;
+
+    private array $options = [];
+
+    private UrlGeneratorInterface $urlGenerator;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    protected function run(): string
+    {
+        if (!isset($this->options['id'])) {
+            $this->options['id'] = "{$this->getId()}-post-card";
+        }
+
+        $this->initOptions();
+
+        $this->registerPlugin('alert', $this->options);
+
+        return Html::beginTag('div', $this->options) . "\n"
+            . Html::beginTag('div', ['class' => 'card-body d-flex flex-column align-items-start']) . "\n"
+            . "\n" . $this->renderHead()
+            . "\n" . $this->renderBody()
+            . "\n" . $this->renderTags()
+            . "\n" . Html::endTag('div')
+            . "\n" . Html::endTag('div');
+    }
+
+    protected function renderHead(): string
+    {
+        return Html::a(
+            Html::encode($this->post->getTitle()),
+            $this->urlGenerator->generate('blog/page', ['slug' => $this->post->getSlug()]),
+            ['class' => 'mb-0 h4 text-decoration-none'] // stretched-link
+        );
+    }
+
+    protected function renderBody(): string
+    {
+        return Html::tag(
+            'div',
+            $this->post->getPublishedAt()->format('M, d') . ' by ' . Html::a(
+                Html::encode($this->post->getUser()->getLogin()),
+                $this->urlGenerator->generate('user/profile', ['login' => $this->post->getUser()->getLogin()])
+            ),
+            ['class' => 'mb-1 text-muted']
+        ) . Html::tag(
+            'p',
+            Html::encode(mb_substr($this->post->getContent(), 0, 400))
+            . (mb_strlen($this->post->getContent()) > 400 ? '…' : ''),
+            ['class' => 'card-text mb-auto']
+        );
+    }
+
+    protected function renderTags(): string
+    {
+        $return = Html::beginTag('div', ['class' => 'mt-3']);
+        foreach ($this->post->getTags() as $tag) {
+            $return .= Html::a(
+                Html::encode($tag->getLabel()),
+                $this->urlGenerator->generate('blog/tag', ['label' => $tag->getLabel()]),
+                ['class' => 'btn btn-outline-secondary btn-sm mx-1 mt-1']
+            );
+        }
+        return $return . Html::endTag('div');
+    }
+
+    protected function initOptions(): void
+    {
+        Html::addCssClass($this->options, ['widget' => 'card mb-4']);
+    }
+
+    public function post(?Post $post): self
+    {
+        $this->post = $post;
+
+        if ($post !== null) {
+            $this->options['data']['post-slug'] = $post->getSlug();
+        }
+
+        return $this;
+    }
+
+    /**
+     * The HTML attributes for the widget container tag. The following special options are recognized.
+     *
+     * {@see \Yiisoft\Html\Html::renderTagAttributes()} for details on how attributes are being rendered.
+     */
+    public function options(array $value): self
+    {
+        $this->options = $value;
+
+        return $this;
+    }
+}
--- views/blog/index.php
+++ views/blog/index.php
@@ -28,44 +28,7 @@ use Yiisoft\Html\Html;
         }
         /** @var Post $item */
         foreach ($paginationSet->getPaginator()->read() as $item) {
-            $url = $urlGenerator->generate('blog/page', ['slug' => $item->getSlug()]);
-
-            echo Html::beginTag('div', ['class' => 'card mb-4']);
-            echo Html::beginTag('div', ['class' => 'card-body d-flex flex-column align-items-start']);
-
-            echo Html::a(
-                Html::encode($item->getTitle()),
-                $url,
-                ['class' => 'mb-0 h4 text-decoration-none'] // stretched-link
-            );
-            echo Html::tag(
-                'div',
-                $item->getPublishedAt()->format('M, d') . ' by ' . Html::a(
-                    Html::encode($item->getUser()->getLogin()),
-                    $urlGenerator->generate('user/profile', ['login' => $item->getUser()->getLogin()])
-                ),
-                ['class' => 'mb-1 text-muted']
-            );
-            echo Html::tag(
-                'p',
-                Html::encode(mb_substr($item->getContent(), 0, 400))
-                    . (mb_strlen($item->getContent()) > 400 ? '…' : ''),
-                ['class' => 'card-text mb-auto']
-            );
-            if ($item->getTags()->count()) {
-                echo Html::beginTag('div', ['class' => 'mt-3']);
-                foreach ($item->getTags() as $tag) {
-                    echo Html::a(
-                        Html::encode($tag->getLabel()),
-                        $urlGenerator->generate('blog/tag', ['label' => $tag->getLabel()]),
-                        ['class' => 'btn btn-outline-secondary btn-sm mx-1 mt-1']
-                    );
-                }
-                echo Html::endTag('div');
-            }
-
-            echo Html::endTag('div'); # .card-body
-            echo Html::endTag('div'); # .card
+            echo \App\Blog\Widget\PostCard::widget()->post($item);
         }
         if ($paginationSet->needToPaginate()) {
             echo $this->render('_pagination', ['paginationSet' => $paginationSet]);
