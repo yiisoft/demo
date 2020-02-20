2e605e15d6aa383a4f73681522a5142c8ca3b876
Dmitriy Derepko <xepozz@list.ru>
2020-02-20 14:32:46 +0300

    Added debugger

--- public/index.php
+++ public/index.php
@@ -10,9 +10,17 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 // Don't do it in production, assembling takes it's time
 Builder::rebuild();
 
-$container = new Container(require Builder::path('web'));
+$debugProvider = new \Yiisoft\Yii\Debug\DebugServiceProvider();
+$container = new Container(require Builder::path('web'), [$debugProvider]);
 
 require dirname(__DIR__) . '/src/globals.php';
 
-$request = $container->get(ServerRequestFactory::class)->createFromGlobals();
-$container->get(Application::class)->handle($request);
+$dispatcher = $container->get(\Psr\EventDispatcher\EventDispatcherInterface::class);
+$event = $dispatcher->dispatch(new \Yiisoft\Yii\Debug\Event\ApplicationStartup());
+
+try {
+    $request = $container->get(ServerRequestFactory::class)->createFromGlobals();
+    $container->get(Application::class)->handle($request);
+} finally {
+    $dispatcher->dispatch(new \Yiisoft\Yii\Debug\Event\ApplicationShutdown());
+}
