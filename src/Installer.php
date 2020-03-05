e12b166c69fe30a9515042562b0885bc62c7ff7d
Anton Samoylenko <a.samoylenko95@gmail.com>
2020-03-05 10:48:03 +0200

    Installer codestyle fixes

--- src/Installer.php
+++ src/Installer.php
@@ -3,21 +3,20 @@
 namespace App;
 
 use Composer\Script\Event;
-use Composer\Installer\PackageEvent;
 
 class Installer
 {
-    public static function postInstall(Event $event): void 
+    public static function postInstall(Event $event): void
     {
         self::rchmod('runtime', 0777);
     }
 
     private static function rchmod(string $path, int $mode): void {
-        $dir = new \DirectoryIterator($path); 
-        foreach ($dir as $item) { 
-            chmod($item->getPathname(), $mode); 
-            if ($item->isDir() && !$item->isDot()) { 
-                self::rchmod($item->getPathname(), $mode); 
+        $dir = new \DirectoryIterator($path);
+        foreach ($dir as $item) {
+            chmod($item->getPathname(), $mode);
+            if ($item->isDir() && !$item->isDot()) {
+                self::rchmod($item->getPathname(), $mode);
             }
         }
     }
