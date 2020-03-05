27d1a043ecd7dd9edbbaebbab1e1c46cf28e9e2b
Anton Samoylenko <a.samoylenko95@gmail.com>
2020-03-05 10:26:02 +0200

    Add Installer

---
+++ src/Installer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+class Installer
+{
+    public static function postInstall(Event $event): void 
+    {
+        self::rchmod('runtime', 0777);
+    }
+
+    private static function rchmod(string $path, int $mode): void {
+        $dir = new \DirectoryIterator($path); 
+        foreach ($dir as $item) { 
+            chmod($item->getPathname(), $mode); 
+            if ($item->isDir() && !$item->isDot()) { 
+                self::rchmod($item->getPathname(), $mode); 
+            }
+        }
+    }
+}
