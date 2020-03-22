485236e05ccdcd57335b5ec88db899064004119e
Alexander Makarov <sam@rmcreative.ru>
2020-03-22 22:11:46 +0300

    Add CSRF protection

--- src/Factory/MiddlewareDispatcherFactory.php
+++ src/Factory/MiddlewareDispatcherFactory.php
@@ -5,6 +5,7 @@ namespace App\Factory;
 use Psr\Container\ContainerInterface;
 use Yiisoft\Router\Middleware\Router;
 use Yiisoft\Yii\Web\ErrorHandler\ErrorCatcher;
+use Yiisoft\Yii\Web\Middleware\Csrf;
 use Yiisoft\Yii\Web\Middleware\SubFolder;
 use Yiisoft\Yii\Web\MiddlewareDispatcher;
 use Yiisoft\Yii\Web\Session\SessionMiddleware;
@@ -13,12 +14,14 @@ final class MiddlewareDispatcherFactory
 {
     public function __invoke(ContainerInterface $container)
     {
+        $csrf = $container->get(Csrf::class);
         $session = $container->get(SessionMiddleware::class);
         $router = $container->get(Router::class);
         $errorCatcher = $container->get(ErrorCatcher::class);
         $subFolder = $container->get(SubFolder::class);
 
         return (new MiddlewareDispatcher($container))
+            ->addMiddleware($csrf)
             ->addMiddleware($router)
             ->addMiddleware($subFolder)
             ->addMiddleware($session)
