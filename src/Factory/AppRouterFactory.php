208c1f9155d039d7dcdf766b72523748380d8343
yiiliveext <yiiliveext@gmail.com>
2020-03-29 18:20:37 +0300

    Update api data wrapper

--- src/Factory/AppRouterFactory.php
+++ src/Factory/AppRouterFactory.php
@@ -13,6 +13,7 @@ use App\Controller\ContactController;
 use App\Controller\SiteController;
 use App\Controller\UserController;
 use App\JsonResponseFormatter;
+use App\Response;
 use App\ResponseFactory;
 use App\ResponseFormatter;
 use App\DeferredResponseFormatter;
@@ -58,7 +59,7 @@ class AppRouterFactory
                     return $responseFactory->createResponse(200, '', ['version' => '1.0', 'author' => 'yiiliveext']);
                 }),
                 Route::get('/info/v2', ApiInfo::class)
-                    ->addMiddleware(new DeferredResponseFormatter($container->get(JsonResponseFormatter::class))),
+                    ->addMiddleware(new ResponseFormatter($container->get(JsonResponseFormatter::class))),
                 Route::get('/user', [ApiUserController::class, 'index'])
                     ->name('api/user/index'),
                 Route::get('/user/{login}', [ApiUserController::class, 'profile'])
@@ -66,19 +67,22 @@ class AppRouterFactory
                     ->name('api/user/profile'),
             ], $container)->addMiddleware(function (ServerRequestInterface $request, RequestHandlerInterface $handler) {
                 $response = $handler->handle($request);
-                $data = $response->getData();
-                if ($response->getStatusCode() !== 200) {
-                    if (!empty($data)) {
-                        $message = $data;
-                    } else {
-                        $message = 'Unknown error';
+                if ($response instanceof Response) {
+                    $data = $response->getData();
+                    if ($response->getStatusCode() !== 200) {
+                        if (!empty($data)) {
+                            $message = $data;
+                        } else {
+                            $message = 'Unknown error';
+                        }
+                        return $response->withData(
+                            ['status' => 'failed',
+                                'error' => ['message' => $message, 'status' => $response->getStatusCode()]]);
                     }
-                    return $response->withData(
-                        ['status' => 'failed',
-                        'error' => ['message' => $message, 'status' => $response->getStatusCode()]]);
+                    return $response->withData(['status' => 'success', 'data' => $data]);
                 }
 
-                return $response->withData(['status' => 'success', 'data' => $data]);
+                return $response;
             })->addMiddleware(new DeferredResponseFormatter($container->get(XmlResponseFormatter::class))),
 
             // Blog routes
