63e395ae14b8036cf0edf12c9573d14fdce1ab9d
yiiliveext <yiiliveext@gmail.com>
2020-03-29 18:17:53 +0300

    Add api info v2

---
+++ src/Controller/ApiInfo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\ResponseFactory;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ApiInfo implements MiddlewareInterface
+{
+    private ResponseFactory $responseFactory;
+
+    public function __construct(ResponseFactory $responseFactory)
+    {
+        $this->responseFactory = $responseFactory;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $this->responseFactory->createResponse(200, '', ['version' => '2.0', 'author' => 'yiiliveext']);
+    }
+}
--- src/Factory/AppRouterFactory.php
+++ src/Factory/AppRouterFactory.php
@@ -6,6 +6,7 @@ use App\Blog\Archive\ArchiveController;
 use App\Blog\BlogController;
 use App\Blog\Post\PostController;
 use App\Blog\Tag\TagController;
+use App\Controller\ApiInfo;
 use App\Controller\ApiUserController;
 use App\Controller\AuthController;
 use App\Controller\ContactController;
@@ -53,9 +54,11 @@ class AppRouterFactory
 
             // User
             Group::create('/api', [
-                Route::get('/info', function (ResponseFactory $responseFactory) {
+                Route::get('/info/v1', function (ResponseFactory $responseFactory) {
                     return $responseFactory->createResponse(200, '', ['version' => '1.0', 'author' => 'yiiliveext']);
                 }),
+                Route::get('/info/v2', ApiInfo::class)
+                    ->addMiddleware(new DeferredResponseFormatter($container->get(JsonResponseFormatter::class))),
                 Route::get('/user', [ApiUserController::class, 'index'])
                     ->name('api/user/index'),
                 Route::get('/user/{login}', [ApiUserController::class, 'profile'])
