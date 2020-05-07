c5319f483df3094a47c44a0305bffef282ee03dc
Rustam Mamadaminov <rmamdaminov@gmail.com>
2020-05-07 12:31:10 +0500

    fix

--- src/Command/Route/ListCommand.php
+++ src/Command/Route/ListCommand.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Command\Route;
 
-
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Helper\TableSeparator;
