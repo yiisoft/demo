11bbee0819c38f3d0dcb58d4d23f0e484ea8c808
Alexander Makarov <sam@rmcreative.ru>
2020-08-15 17:24:07 +0300

    Use require instead of variable

--- config/providers-web.php
+++ config/providers-web.php
@@ -1,12 +1,13 @@
 <?php
 
 use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
+use Yiisoft\Composer\Config\Builder;
 use Yiisoft\Yii\Event\EventDispatcherProvider;
 
 return [
     ReverseBlockMerge::class => new ReverseBlockMerge(),
     'yiisoft/event-dispatcher/eventdispatcher' => [
         '__class' => EventDispatcherProvider::class,
-        '__construct()' => [$config['events-web']],
+        '__construct()' => [Builder::require('events-web')],
     ],
 ];
