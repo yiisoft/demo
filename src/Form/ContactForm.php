5876d81defac4ed5b64366277c85d26d7e73529b
Anton Samoylenko <a.samoylenko95@gmail.com>
2020-10-04 13:05:24 +0300

    Add form

---
+++ src/Form/ContactForm.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Yiisoft\Form\FormModel;
+use Yiisoft\Validator\Rule\Email;
+use Yiisoft\Validator\Rule\Required;
+
+final class ContactForm extends FormModel
+{
+    private string $username = '';
+    private string $email = '';
+    private string $subject = '';
+    private string $body = '';
+    private ?array $attachFiles = null;
+
+    public function attributeLabels(): array
+    {
+        return [
+            'username' => 'Username',
+            'email' => 'Email',
+            'subject' => 'Subject',
+            'body' => 'Body',
+        ];
+    }
+
+    public function formName(): string
+    {
+        return 'ContactForm';
+    }
+
+    public function rules(): array
+    {
+        return [
+            'username' => [new Required()],
+            'email' => [new Required(), new Email()],
+            'subject' => [new Required()],
+            'body' => [new Required()],
+        ];
+    }
+}
--- views/contact/form.php
+++ views/contact/form.php
@@ -7,50 +7,57 @@ declare(strict_types=1);
  * @var $urlGenerator \Yiisoft\Router\UrlGeneratorInterface
  * @var $body array
  * @var $csrf string
+ * @var $form Yiisoft\Form\FormModel
+ * @var $url \Yiisoft\Router\UrlGeneratorInterface
+ * @var $field \Yiisoft\Form\Widget\Field
  */
 
+use Yiisoft\Form\Widget\Form;
 use Yiisoft\Html\Html;
 use Yiisoft\Yii\Bootstrap4\Alert;
 
 if (isset($sent)) {
     echo Alert::widget()
-              ->options(['class' => $sent ? 'alert-success' : 'alert-danger'])
+              ->options(['class' => !$form->hasErrors() ? 'alert-success' : 'alert-danger'])
               ->body(
-                  $sent
+                  $sent && !$form->hasErrors()
                       ? 'Thanks to contact us, we\'ll get in touch with you as soon as possible.'
-                      : Html::encode($error)
+                      : 'Some values is incorrect'
               );
 }
 ?>
-<form id="contactForm"
-      method="POST"
-      action="<?= $urlGenerator->generate('site/contact') ?>"
-      enctype="multipart/form-data"
->
-    <input type="hidden" name="_csrf" value="<?= $csrf ?>">
-    <div class="form-group">
-        <label for="subject">Subject</label>
-        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject"
-               value="<?= Html::encode($body['subject'] ?? '') ?>" required>
-    </div>
-    <div class="form-group">
-        <label for="email">Email</label>
-        <input type="email" class="form-control" name="email" id="email" placeholder="Email Address"
-               value="<?= Html::encode($body['email'] ?? '') ?>" required>
-    </div>
-    <div class="form-group">
-        <label for="name">Name</label>
-        <input type="text" class="form-control" name="name" id="name" placeholder="Name"
-               value="<?= Html::encode($body['name'] ?? '') ?>" required>
-    </div>
-    <div class="form-group">
-        <label for="content">Content</label>
-        <textarea class="form-control" name="content" id="content"
-                  placeholder="Content" required><?= Html::encode($body['content'] ?? '') ?></textarea>
-    </div>
-    <div class="form-group">
-        <label>File</label>
-        <input type="file" class="form-control-file" name="file">
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<div>
+
+    <?= Form::begin()
+        ->action($url->generate('site/contact'))
+        ->options(
+            [
+                'id' => 'form-contact',
+                'csrf' => $csrf,
+                'enctype' => 'multipart/form-data',
+            ]
+        )
+        ->start() ?>
+
+    <?= $field->config($form, 'username') ?>
+    <?= $field->config($form, 'email') ?>
+    <?= $field->config($form, 'subject') ?>
+    <?= $field->config($form, 'body')
+        ->textArea(['class' => 'form-control textarea', 'rows' => 2]) ?>
+    <?= $field->config($form, 'attachFiles')
+        ->inputCssClass('file-input')
+        ->fileInput(
+            ['type' => 'file', 'multiple' => 'multiple', 'name' => 'attachFiles[]'],
+            true,
+        ) ?>
+
+    <?= Html::submitButton(
+        'Submit',
+        [
+            'class' => 'btn btn-primary'
+        ]
+    ) ?>
+
+    <?= Form::end() ?>
+
+</div>
