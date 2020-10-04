de6d2ce44a9363c330cbdf707612ceb31cc8d063
Dmitriy Derepko <xepozz@list.ru>
2020-10-04 21:40:40 +0300

    Update console.php (#149)

--- config/console.php
+++ config/console.php
@@ -2,15 +2,9 @@
 
 declare(strict_types=1);
 
-use Yiisoft\Aliases\Aliases;
-
 /**
  * @var array $params
  */
 
 return [
-    Aliases::class => [
-        '__class' => Aliases::class,
-        '__construct()' => [$params['aliases']],
-    ],
 ];
