6569a9eae9b05477de37f27fe6e2227ff3ed2f37
Anton Samoylenko <a.samoylenko95@gmail.com>
2020-10-05 18:20:13 +0300

    Revert "Update console.php (#149)"
    This reverts commit de6d2ce44a9363c330cbdf707612ceb31cc8d063.

--- config/console.php
+++ config/console.php
@@ -2,9 +2,15 @@
 
 declare(strict_types=1);
 
+use Yiisoft\Aliases\Aliases;
+
 /**
  * @var array $params
  */
 
 return [
+    Aliases::class => [
+        '__class' => Aliases::class,
+        '__construct()' => [$params['aliases']],
+    ],
 ];
