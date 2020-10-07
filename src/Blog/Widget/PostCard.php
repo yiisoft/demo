5a66878030e68e5166bc2e552a4e96d0b7b4d869
Aleksei Gagarin <roxblnfk@ya.ru>
2020-10-08 00:37:28 +0300

    Replace Yiisoft\Yii\Bootstrap4\Html to Yiisoft\Html\Html (#154)

--- src/Blog/Widget/PostCard.php
+++ src/Blog/Widget/PostCard.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace App\Blog\Widget;
 
 use App\Blog\Entity\Post;
+use Yiisoft\Html\Html;
 use Yiisoft\Router\UrlGeneratorInterface;
-use Yiisoft\Yii\Bootstrap4\Html;
 use Yiisoft\Yii\Bootstrap4\Widget;
 
 class PostCard extends Widget
