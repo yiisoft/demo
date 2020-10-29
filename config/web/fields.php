3c7b49f8f19990a716dbd0651e82035081aa196a
Wilmer Arambula <terabytesoftw@gmail.com>
2020-10-29 14:04:04 -0300

    Fix minor correction.

--- config/web/fields.php
+++ config/web/fields.php
@@ -7,5 +7,5 @@ use Yiisoft\Form\Widget\Field;
 /** @var array $params */
 
 return [
-    Field::class => fn () => Field::Widget($params['yiisoft/form']['fieldConfig'])
+    Field::class => static fn () => Field::Widget($params['yiisoft/form']['fieldConfig'])
 ];
