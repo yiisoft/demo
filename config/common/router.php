18a1d6c8e014bec00f764bf77f34ccb3fb508a3a
yiiliveext <yiiliveext@gmail.com>
2020-11-03 00:48:15 +0200

    Cleanup router config

--- config/common/router.php
+++ config/common/router.php
@@ -8,18 +8,8 @@ use Yiisoft\Router\Group;
 use Yiisoft\Router\RouteCollection;
 use Yiisoft\Router\RouteCollectorInterface;
 use Yiisoft\Router\RouteCollectionInterface;
-use Yiisoft\Router\UrlGeneratorInterface;
-use Yiisoft\Router\UrlMatcherInterface;
-use Yiisoft\Router\FastRoute\UrlGenerator;
-use Yiisoft\Router\FastRoute\UrlMatcher;
 
 return [
-    UrlMatcherInterface::class => UrlMatcher::class,
-
-    UrlGeneratorInterface::class => UrlGenerator::class,
-
-    RouteCollectorInterface::class => Group::create(),
-
     RouteCollectionInterface::class => static function (RouteCollectorInterface $collector) {
         $collector->addGroup(
             Group::create(null, require Builder::path('routes'))
