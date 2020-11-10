f4b8abbef027950eea4b3058521c170568581f5c
Wilmer Arambula <terabytesoftw@gmail.com>
2020-11-10 11:27:53 -0300

    Fix config console.

--- config/providers-web.php
+++ config/providers-web.php
@@ -3,14 +3,7 @@
 declare(strict_types=1);
 
 use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
-use Yiisoft\Composer\Config\Builder;
-use Yiisoft\Yii\Event\EventDispatcherProvider;
 
 return [
-    'yiisoft/event-dispatcher/eventdispatcher' => [
-        '__class' => EventDispatcherProvider::class,
-        '__construct()' => [Builder::require('events-web')]
-    ],
-
     ReverseBlockMerge::class => new ReverseBlockMerge(),
 ];
