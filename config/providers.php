4969c01c55500f7604d2b811844dfd5dac63cef0
Wilmer Arambula <terabytesoftw@gmail.com>
2020-11-10 13:15:16 -0300

    Remove config yii-event.

--- config/providers.php
+++ config/providers.php
@@ -3,14 +3,7 @@
 declare(strict_types=1);
 
 use App\Provider\RepositoryProvider;
-use Yiisoft\Composer\Config\Builder;
-use Yiisoft\Yii\Event\EventDispatcherProvider;
 
 return [
    'RepositoryProvider' => RepositoryProvider::class,
-
-   'yiisoft/event-dispatcher/eventdispatcher' => [
-        '__class' => EventDispatcherProvider::class,
-        '__construct()' => [Builder::require('events-web')]
-    ]
 ];
