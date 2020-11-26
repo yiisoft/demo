7450645444b56b8d6abad8edf01774b1407fa5e9
Alexander Makarov <sam@rmcreative.ru>
2020-11-26 17:42:44 +0300

    Fix config

--- config/web/application.php
+++ config/web/application.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 use Yiisoft\Csrf\CsrfMiddleware;
 use Yiisoft\ErrorHandler\ErrorCatcher;
+use Yiisoft\Factory\Definitions\Reference;
 use Yiisoft\Injector\Injector;
 use Yiisoft\Middleware\Dispatcher\MiddlewareDispatcher;
 use Yiisoft\Router\Middleware\Router;
 use Yiisoft\Session\SessionMiddleware;
+use Yiisoft\Yii\Web\NotFoundHandler;
 
 return [
     Yiisoft\Yii\Web\Application::class => [
@@ -23,6 +25,7 @@ return [
                         ]
                     );
             },
+            'notFoundHandler' => Reference::to(NotFoundHandler::class),
         ],
     ],
 ];
