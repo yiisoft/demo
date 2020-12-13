f995a607a1b30442a6cb3e18bafbd90d463dc832
Mr.42 <johan@mr42.me>
2020-12-13 12:25:22 +0100

    Removing svg data, bootstrap already has a close/delete cross icon

--- public/js/app.js
+++ public/js/app.js
@@ -28,19 +28,7 @@ document.addEventListener('click', function (event) {
             tags.insertAdjacentHTML('beforeend',
                 `<button type="button" class="btn btn-sm btn-info mt-3 me-2" onclick="this.remove()">
                     <input type="hidden" name="tags[]" value="` + input.value + `">
-                    ` + input.value + ` <span class="badge bg-dark">
-                    <svg
-                    width="2em"
-                    height="2em"
-                    viewBox="0 0 16 16" class="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                      fill-rule="evenodd"
-                      d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647
-                      2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646
-                      5.354a.5.5 0 0 1 0-.708z"/>
-                    </svg>
-                    </span>
+                    ` + input.value + `<span class="btn-close ms-1"></span>
                 </button>`);
             input.value = '';
         }
