135b81e1e69ca0baf5dbca717df27d4b42505eae
viktorprogger <viktorprogger@gmail.com>
2020-12-22 13:34:13 +0300

    Remove yii-event package

---
+++ config/console/events.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\Composer\Config\Builder;
+use Yiisoft\EventDispatcher\Provider\ListenerCollection;
+use Yiisoft\EventDispatcher\Support\ListenerCollectionFactory;
+
+return [
+    ListenerCollection::class => static fn (ListenerCollectionFactory $factory) => $factory->create(require Builder::path('events-console')),
+];
