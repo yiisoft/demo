d84f30d01636028e558ac5007c578280160090b5
1Luc1 <lukas.aigner@gmail.com>
2020-12-25 09:46:18 +0100

    Bootstrap5 adjustments (#208)
    * adjusted badge classes
    * removed dropped form-group class
    * fixed label for attribute
    * added missing form-label class

--- views/blog/post/__form.php
+++ views/blog/post/__form.php
@@ -24,41 +24,34 @@ if (!empty($errors)) {
 
 <h1><?= Html::encode($title) ?></h1>
 
-<form id="postForm"
-      method="POST"
-      action="<?= $urlGenerator->generate(...$action) ?>"
-      enctype="multipart/form-data"
->
+<form id="postForm" method="POST" action="<?= $urlGenerator->generate(...$action) ?>" enctype="multipart/form-data">
     <input type="hidden" name="_csrf" value="<?= $csrf ?>">
-    <div class="form-group">
-        <label for="title" class="required">Title</label>
-        <input type="text" class="form-control" name="title" id="title" placeholder="Title"
-               value="<?= Html::encode($body['title'] ?? '') ?>" required>
+    <div class="mb-3">
+        <label for="title" class="form-label required">Title</label>
+        <input type="text" class="form-control" name="title" id="title" placeholder="Title" value="<?= Html::encode($body['title'] ?? '') ?>" required>
     </div>
 
-    <div class="form-group mt-3">
-        <label for="content" class="required">Content</label>
-        <textarea class="form-control" name="content" id="content"
-                  placeholder="Content" required><?= Html::encode($body['content'] ?? '') ?></textarea>
+    <div class="mb-3">
+        <label for="content" class="form-label required">Content</label>
+        <textarea class="form-control" name="content" id="content" placeholder="Content" required><?= Html::encode($body['content'] ?? '') ?></textarea>
     </div>
 
-    <div class="form-group mt-3">
-        <label for="AddTag">Add tag</label>
+    <div class="mb-3">
+        <label for="addTag" class="form-label">Add tag</label>
         <input type="text" class="form-control" id="addTag" placeholder="Add tag" value="">
         <?= Html::button(
-    'Add',
-    ['class' => 'btn btn-primary mt-2', 'id' => 'addTagButton']
-) ?>
+            'Add',
+            ['class' => 'btn btn-primary mt-2', 'id' => 'addTagButton']
+        ) ?>
+        <div id="tags">
+            <?php foreach ($body['tags'] ?? [] as $tag) : ?>
+                <button type="button" class="btn btn-sm btn-info me-2 remove-tag">
+                    <input type="hidden" name="tags[]" value="<?= Html::encode($tag) ?>">
+                    <?= Html::encode($tag) ?><span class="btn-close ms-1"></span>
+                </button>
+            <?php endforeach; ?>
+        </div>
     </div>
 
-    <div class="form-group" id="tags">
-        <?php foreach ($body['tags'] ?? [] as $tag): ?>
-            <button type="button" class="btn btn-sm btn-info mt-3 me-2 remove-tag">
-                <input type="hidden" name="tags[]" value="<?= Html::encode($tag) ?>">
-                <?= Html::encode($tag) ?><span class="btn-close ms-1"></span>
-            </button>
-        <?php endforeach; ?>
-    </div>
-
-    <button type="submit" class="btn btn-primary mt-3">Submit</button>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
--- views/signup/signup.php
+++ views/signup/signup.php
@@ -13,28 +13,29 @@ use Yiisoft\Html\Html;
 $error = $error ?? null;
 ?>
 
-<?php if ($error !== null): ?>
-<div class="alert alert-danger" role="alert">
-  <?= Html::encode($error) ?>
-</div>
+<?php if ($error !== null) : ?>
+  <div class="alert alert-danger" role="alert">
+    <?= Html::encode($error) ?>
+  </div>
 <?php endif ?>
 
 <form id="signupForm" method="POST" action="<?= $urlGenerator->generate('site/signup') ?>" enctype="multipart/form-data">
   <input type="hidden" name="_csrf" value="<?= $csrf ?>">
-  <div class="form-group">
-    <label for="login" class="required">Login</label>
-      <?= Html::textInput('login', $body['login'] ?? '', [
-          'class' => 'form-control',
-          'required' => true,
-      ]) ?>
-  </div>
-  <div class="form-group mt-3">
-    <label for="password" class="required">Password</label>
-      <?= Html::passwordInput('password', $body['password'] ?? '', [
-        'class' => 'form-control',
-        'required' => true,
+  <div class="mb-3">
+    <label for="login" class="form-label required">Login</label>
+    <?= Html::textInput('login', $body['login'] ?? '', [
+      'id' => 'login',
+      'class' => 'form-control',
+      'required' => true,
     ]) ?>
   </div>
-  <button type="submit" class="btn btn-primary mt-3">Submit</button>
+  <div class="mb-3">
+    <label for="password" class="form-label required">Password</label>
+    <?= Html::passwordInput('password', $body['password'] ?? '', [
+      'id' => 'password',
+      'class' => 'form-control',
+      'required' => true,
+    ]) ?>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
-
