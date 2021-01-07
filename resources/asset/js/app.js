a9a7cc3b97f86f4ef1a556ef2a4d28abcf91e355
Julian Böhnke <info@julianb.de>
2021-01-07 13:17:52 +0100

    better string template

--- resources/asset/js/app.js
+++ resources/asset/js/app.js
@@ -8,14 +8,14 @@ document.addEventListener('click', function (event) {
         xhr.open('GET', event.target.href);
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.send();
-        xhr.onreadystatechange = function() {
+        xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
-                if(xhr.status === 200) {
+                if (xhr.status === 200) {
                     event.target.parentNode.parentNode.remove();
                     document.querySelector(".comment-feed-container").insertAdjacentHTML('beforeend', xhr.responseText);
                 } else {
                     event.target.disabled = false;
-                    document.querySelector(".comment-feed-container").insertAdjacentText('beforeend', 'An error occurred during your request: ' +  xhr.status + ': ' + xhr.statusText);
+                    document.querySelector(".comment-feed-container").insertAdjacentText('beforeend', `An error occurred during your request: ${xhr.status}: ${xhr.statusText}`);
                 }
             }
         };
@@ -27,8 +27,8 @@ document.addEventListener('click', function (event) {
             var tags = document.getElementById('tags');
             tags.insertAdjacentHTML('beforeend',
                 `<button type="button" class="btn btn-sm btn-info mt-3 me-2 remove-tag">
-                    <input type="hidden" name="tags[]" value="` + input.value + `">
-                    ` + input.value + `<span class="btn-close ms-1"></span>
+                    <input type="hidden" name="tags[]" value="${input.value}">
+                    ${input.value}<span class="btn-close ms-1"></span>
                 </button>`);
             input.value = '';
         }
