2a8d4e8a4a499d6f8cdd7fa5a71faec46b82b7d3
Julian Böhnke <info@julianb.de>
2021-01-07 13:39:30 +0100

    more modern JS

--- resources/asset/js/app.js
+++ resources/asset/js/app.js
@@ -1,14 +1,15 @@
 // app.js
-document.addEventListener('click', function (event) {
+document.addEventListener('click', event => {
+    console.log("test", event.target.matches);
     if (event.target.matches('.load-more-comment')) {
         event.preventDefault();
         event.target.disabled = true;
 
-        var xhr = new XMLHttpRequest();
+        const xhr = new XMLHttpRequest();
         xhr.open('GET', event.target.href);
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.send();
-        xhr.onreadystatechange = function () {
+        xhr.onreadystatechange = () => {
             if (xhr.readyState === 4) {
                 if (xhr.status === 200) {
                     event.target.parentNode.parentNode.remove();
@@ -22,9 +23,9 @@ document.addEventListener('click', function (event) {
     }
 
     if (event.target.matches('#addTagButton')) {
-        var input = document.getElementById('addTag');
-        if (input.value && !document.getElementById('tag' + input.value)) {
-            var tags = document.getElementById('tags');
+        const input = document.getElementById('addTag');
+        if (input.value && !document.getElementById(`tag ${input.value}`)) {
+            const tags = document.getElementById('tags');
             tags.insertAdjacentHTML('beforeend',
                 `<button type="button" class="btn btn-sm btn-info mt-3 me-2 remove-tag">
                     <input type="hidden" name="tags[]" value="${input.value}">
