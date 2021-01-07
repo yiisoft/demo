e22503a8facd6bc58b5ac6f303e3ebe1a195a491
Mr.42 <johan@mr42.me>
2021-01-07 20:22:36 +0100

    Move to events

--- config/events-console.php
+++ config/events-console.php
@@ -2,13 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Timer;
 use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
-use Yiisoft\Yii\Console\Event\ApplicationStartup;
 
 return [
-    ApplicationStartup::class => [
-        static fn (Timer $timer) => $timer->start('overall'),
-    ],
     ReverseBlockMerge::class => new ReverseBlockMerge(),
 ];
--- config/events-web.php
+++ config/events-web.php
@@ -2,13 +2,8 @@
 
 declare(strict_types=1);
 
-use App\Timer;
 use Yiisoft\Arrays\Modifier\ReverseBlockMerge;
-use Yiisoft\Yii\Web\Event\ApplicationStartup;
 
 return [
-    ApplicationStartup::class => [
-        static fn (Timer $timer) => $timer->start('overall'),
-    ],
     ReverseBlockMerge::class => new ReverseBlockMerge(),
 ];
