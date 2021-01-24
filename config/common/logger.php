743fcd153b716f9853dcf1b6838c4108b6bd63d4
Evgeniy Zyubin <mail@devanych.ru>
2021-01-24 19:55:26 +0300

    Replace DbTarget on FileTarget

--- config/common/logger.php
+++ config/common/logger.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 use Psr\Log\LoggerInterface;
 use Yiisoft\Log\Logger;
-use Yiisoft\Log\Target\Db\DbTarget;
+use Yiisoft\Log\Target\File\FileTarget;
 
 return [
-    LoggerInterface::class => static fn (DbTarget $dbTarget) => new Logger([$dbTarget]),
+    LoggerInterface::class => static fn (FileTarget $fileTarget) => new Logger([$fileTarget]),
 ];
