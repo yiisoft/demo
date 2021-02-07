6b7388798f0fab3a38e49af10e24c9353e10ff7a
Evgeniy Zyubin <mail@devanych.ru>
2021-02-07 12:49:18 +0300

    Remove dump functions from this package (#235)

--- src/functions.php
+++ src/functions.php
@@ -3,7 +3,6 @@
 declare(strict_types=1);
 
 use Yiisoft\Files\FileHelper;
-use Yiisoft\VarDumper\VarDumper;
 
 function shouldRebuildConfigs(): bool
 {
@@ -17,23 +16,4 @@ function shouldRebuildConfigs(): bool
     $sourceTime = FileHelper::lastModifiedTime($sourceDirectory);
     $buildTime = FileHelper::lastModifiedTime($buildDirectory);
     return $buildTime < $sourceTime;
-}
-
-if (!function_exists('d')) {
-    function d(...$variables)
-    {
-        foreach ($variables as $variable) {
-            VarDumper::dump($variable, 10, PHP_SAPI !== 'cli');
-        }
-    }
-}
-
-if (!function_exists('dd')) {
-    function dd(...$variables)
-    {
-        foreach ($variables as $variable) {
-            VarDumper::dump($variable, 10, PHP_SAPI !== 'cli');
-        }
-        die();
-    }
 }
