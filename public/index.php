672c14f1b9cbb3b9491621006366e32eefa0486f
Evgeniy Zyubin <mail@devanych.ru>
2021-02-24 16:41:24 +0300

    Add ApplicationRunner and delete src/functions.php

--- public/index.php
+++ public/index.php
@@ -2,16 +2,7 @@
 
 declare(strict_types=1);
 
-use Psr\Container\ContainerInterface;
-use Psr\Log\NullLogger;
-use Yiisoft\Composer\Config\Builder;
-use Yiisoft\Di\Container;
-use Yiisoft\ErrorHandler\ErrorHandler;
-use Yiisoft\ErrorHandler\Renderer\HtmlRenderer;
-use Yiisoft\Http\Method;
-use Yiisoft\Yii\Web\Application;
-use Yiisoft\Yii\Web\SapiEmitter;
-use Yiisoft\Yii\Web\ServerRequestFactory;
+use App\ApplicationRunner;
 
 // PHP built-in server routing.
 if (PHP_SAPI === 'cli-server') {
@@ -26,47 +17,8 @@ if (PHP_SAPI === 'cli-server') {
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-// Don't do it in production, assembling takes it's time
-if (shouldRebuildConfigs()) {
-    Builder::rebuild();
-}
-
-$startTime = microtime(true);
-
-/**
- * Register temporary error handler to catch error while container is building.
- */
-$errorHandler = new ErrorHandler(new NullLogger(), new HtmlRenderer());
+$runner = new ApplicationRunner();
 // Development mode:
-$errorHandler->debug();
-$errorHandler->register();
-
-$container = new Container(
-    require Builder::path('web'),
-    require Builder::path('providers-web')
-);
-
-/**
- * Configure error handler with real container-configured dependencies.
- */
-$errorHandler->unregister();
-$errorHandler = $container->get(ErrorHandler::class);
-// Development mode:
-$errorHandler->debug();
-$errorHandler->register();
-
-$container = $container->get(ContainerInterface::class);
-$application = $container->get(Application::class);
-
-$request = $container->get(ServerRequestFactory::class)->createFromGlobals();
-$request = $request->withAttribute('applicationStartTime', $startTime);
-
-try {
-    $application->start();
-    $response = $application->handle($request);
-    $emitter = new SapiEmitter();
-    $emitter->emit($response, $request->getMethod() === Method::HEAD);
-} finally {
-    $application->afterEmit($response ?? null);
-    $application->shutdown();
-}
+$runner->debug();
+// Run application:
+$runner->run();
