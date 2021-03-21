b30a2af2ed39ef7952744c5212f4a77e883450ef
Alexander Makarov <sam@rmcreative.ru>
2021-03-22 00:52:24 +0300

    Update configs

--- config/packages/yiisoft/data-response/config/web.php
+++ config/packages/yiisoft/data-response/config/web.php
@@ -6,10 +6,22 @@ use Yiisoft\DataResponse\DataResponseFactory;
 use Yiisoft\DataResponse\DataResponseFactoryInterface;
 use Yiisoft\DataResponse\DataResponseFormatterInterface;
 use Yiisoft\DataResponse\Formatter\HtmlDataResponseFormatter;
+use Yiisoft\DataResponse\Formatter\XmlDataResponseFormatter;
+use Yiisoft\DataResponse\Formatter\JsonDataResponseFormatter;
+use Yiisoft\DataResponse\Middleware\ContentNegotiator;
 
 /* @var $params array */
 
 return [
     DataResponseFormatterInterface::class => HtmlDataResponseFormatter::class,
     DataResponseFactoryInterface::class => DataResponseFactory::class,
+    ContentNegotiator::class => [
+        '__construct()' => [
+            'contentFormatters' => [
+                'text/html' => new HtmlDataResponseFormatter(),
+                'application/xml' => new XmlDataResponseFormatter(),
+                'application/json' => new JsonDataResponseFormatter(),
+            ],
+        ],
+    ],
 ];
