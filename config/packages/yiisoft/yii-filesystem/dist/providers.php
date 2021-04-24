819d46983b11990fc3a41ff446c983b2eaffc037
devanych <mail@devanych.ru>
2021-04-24 23:44:39 +0300

    Remove unnecessary dist directories from configs

--- config/packages/yiisoft/yii-filesystem/dist/providers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Yiisoft\Yii\Filesystem\FileStorageServiceProvider;
-
-return [
-    'yiisoft/yii-filesystem/file-storage' => FileStorageServiceProvider::class,
-];
