93800470befc3df02a327cda6116dbf560d4f225
Wilmer <terabytesoftw@gmail.com>
2021-09-08 10:59:03 -0300

    Add params to runner classes.

--- public/index-test.php
+++ public/index-test.php
@@ -21,12 +21,21 @@ if (PHP_SAPI === 'cli-server') {
     $_SERVER['SCRIPT_NAME'] = '/index-test.php';
 }
 
-define('YII_ENV', getenv('env') ?? 'test');
-
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$runner = new WebApplicationRunner();
-// Development mode:
-$runner->debug();
-// Run application:
+/**
+ *  Set debug value for web application runner, for default its `true` add additionally the validation of the
+ *  container-di configurations (debug mode).
+ */
+define('YII_DEBUG', getenv('YII_DEBUG') ?: true);
+
+/**
+ *  Set environment value for web application runner, for default its `null`.
+ *
+ *  @link https://github.com/yiisoft/config#environments
+ */
+define('YII_ENV', getenv('YII_ENV') ?: null);
+
+// Run web application runner
+$runner = new WebApplicationRunner(YII_DEBUG, YII_ENV);
 $runner->run();
--- public/index.php
+++ public/index.php
@@ -15,12 +15,21 @@ if (PHP_SAPI === 'cli-server') {
     $_SERVER['SCRIPT_NAME'] = '/index.php';
 }
 
-define('YII_ENV', getenv('env') ?? 'production');
-
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-$runner = new WebApplicationRunner();
-// Development mode:
-$runner->debug();
-// Run application:
+/**
+ *  Set debug value for web application runner, for default its `true` add additionally the validation of the
+ *  container-di configurations (debug mode).
+ */
+define('YII_DEBUG', getenv('YII_DEBUG') ?: true);
+
+/**
+ *  Set environment value for web application runner, for default its `null`.
+ *
+ *  @link https://github.com/yiisoft/config#environments
+ */
+define('YII_ENV', getenv('YII_ENV') ?: null);
+
+// Run web application runner
+$runner = new WebApplicationRunner(YII_DEBUG, YII_ENV);
 $runner->run();
