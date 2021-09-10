55e36772ec4378f3fc78d03b47d6469359015818
Rustam Mamadaminov <rmamdaminov@gmail.com>
2021-09-10 13:04:35 +0500

    Fix built-in server routing (#352)

--- public/index.php
+++ public/index.php
@@ -7,7 +7,8 @@ use App\Runner\WebApplicationRunner;
 // PHP built-in server routing.
 if (PHP_SAPI === 'cli-server') {
     // Serve static files as is.
-    if (is_file(__DIR__ . $_SERVER['REQUEST_URI'])) {
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    if (is_file(__DIR__ . $path)) {
         return false;
     }
 
