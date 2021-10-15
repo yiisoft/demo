b19d43e48f6e4ed0e64071affcd70754d9b1ff3a
Valerii Gorbachev <darkdef@gmail.com>
2021-10-15 20:19:29 +0300

    Fix rbac storages

--- src/User/Console/CreateCommand.php
+++ src/User/Console/CreateCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Yiisoft\Rbac\Manager;
-use Yiisoft\Rbac\StorageInterface;
+use Yiisoft\Rbac\RolesStorageInterface;
 use Yiisoft\Yii\Console\ExitCode;
 use Yiisoft\Yii\Cycle\Command\CycleDependencyProxy;
 use Yiisoft\Yii\Cycle\Data\Writer\EntityWriter;
@@ -20,15 +20,15 @@ class CreateCommand extends Command
 {
     private CycleDependencyProxy $promise;
     private Manager $manager;
-    private StorageInterface $storage;
+    private RolesStorageInterface $rolesStorage;
 
     protected static $defaultName = 'user/create';
 
-    public function __construct(CycleDependencyProxy $promise, Manager $manager, StorageInterface $storage)
+    public function __construct(CycleDependencyProxy $promise, Manager $manager, RolesStorageInterface $rolesStorage)
     {
         $this->promise = $promise;
         $this->manager = $manager;
-        $this->storage = $storage;
+        $this->rolesStorage = $rolesStorage;
         parent::__construct();
     }
 
@@ -55,7 +55,7 @@ class CreateCommand extends Command
             (new EntityWriter($this->promise->getORM()))->write([$user]);
 
             if ($isAdmin) {
-                $role = $this->storage->getRoleByName('admin');
+                $role = $this->rolesStorage->getRoleByName('admin');
                 $userId = $user->getId();
 
                 if ($role === null) {
