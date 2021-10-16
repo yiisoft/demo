0ae717b7610d20e82039204d117790dcbc8c385b
Valerii Gorbachev <darkdef@gmail.com>
2021-10-16 09:54:05 +0300

    Fix RBAC storages (#366)

--- config/common/rbac.php
+++ config/common/rbac.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 use Yiisoft\Access\AccessCheckerInterface;
 use Yiisoft\Rbac\Manager;
-use Yiisoft\Rbac\Php\Storage;
+use Yiisoft\Rbac\Php\AssignmentsStorage;
+use Yiisoft\Rbac\Php\RolesStorage;
 use Yiisoft\Rbac\RuleFactory\ClassNameRuleFactory;
 use Yiisoft\Rbac\RuleFactoryInterface;
-use Yiisoft\Rbac\StorageInterface;
+use Yiisoft\Rbac\AssignmentsStorageInterface;
+use Yiisoft\Rbac\RolesStorageInterface;
 
 /** @var array $params */
 
 return [
-    StorageInterface::class => [
-        'class' => Storage::class,
+    RolesStorageInterface::class => [
+        'class' => RolesStorage::class,
+        '__construct()' => [
+            'directory' => $params['yiisoft/aliases']['aliases']['@root'] . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'rbac',
+        ],
+    ],
+    AssignmentsStorageInterface::class => [
+        'class' => AssignmentsStorage::class,
         '__construct()' => [
             'directory' => $params['yiisoft/aliases']['aliases']['@root'] . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'rbac',
         ],
