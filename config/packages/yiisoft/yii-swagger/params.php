03ef88e88ee2f078f22e18bf3a4296b886c4caf5
Alexey Chernousov <iamsaint@yandex.ru>
2021-10-22 12:19:22 +0300

    Add persist authorization for SwaggerUI (#363)

---
+++ config/packages/yiisoft/yii-swagger/params.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'yiisoft/yii-swagger' => [
+        'ui-params' => [
+            'persistAuthorization' => true,
+        ],
+    ],
+];
---
+++ config/packages/yiisoft/yii-swagger/web.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\Swagger\Middleware\SwaggerUI;
+
+return [
+    SwaggerUI::class => [
+        '__construct()' => [
+            'params' => $params['yiisoft/yii-swagger']['ui-params'],
+        ],
+    ],
+];
