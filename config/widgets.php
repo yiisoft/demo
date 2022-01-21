daec04200eed912f7a9c5e3b91d0d20d7680e8f3
Wilmer Arambula <42547589+terabytesoftw@users.noreply.github.com>
2022-01-21 08:57:19 -0300

    Apply changes from `yiisoft/form` (#432)

--- config/widgets.php
+++ config/widgets.php
@@ -3,17 +3,23 @@
 declare(strict_types=1);
 
 use Yiisoft\Form\Widget\Field;
+use Yiisoft\Form\Widget\Form;
 
 return [
     Field::class => [
-        'ariaDescribedBy()' => [],
-        'containerClass()' => $params['yiisoft/forms']['containerClass'],
-        'errorClass()' => $params['yiisoft/forms']['errorClass'],
-        'hintClass()' => $params['yiisoft/forms']['hintClass'],
-        'inputClass()' => $params['yiisoft/forms']['inputClass'],
-        'invalidClass()' => $params['yiisoft/forms']['invalidClass'],
-        'labelClass()' => $params['yiisoft/forms']['labelClass'],
-        'template()' => $params['yiisoft/forms']['template'],
-        'validClass()' => $params['yiisoft/forms']['validClass'],
+        'ariaDescribedBy()' => $params['yiisoft/forms']['field']['ariaDescribedBy'],
+        'containerClass()' => $params['yiisoft/forms']['field']['containerClass'],
+        'defaultValues()' => $params['yiisoft/forms']['field']['defaultValues'],
+        'errorClass()' => $params['yiisoft/forms']['field']['errorClass'],
+        'hintClass()' => $params['yiisoft/forms']['field']['hintClass'],
+        'inputClass()' => $params['yiisoft/forms']['field']['inputClass'],
+        'invalidClass()' => $params['yiisoft/forms']['field']['invalidClass'],
+        'labelClass()' => $params['yiisoft/forms']['field']['labelClass'],
+        'template()' => $params['yiisoft/forms']['field']['template'],
+        'validClass()' => $params['yiisoft/forms']['field']['validClass'],
+    ],
+
+    Form::class => [
+        'attributes()' => $params['yiisoft/forms']['form']['attributes'],
     ],
 ];
