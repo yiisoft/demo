7d8cb530e6b65072b43e156b78d2b4407f76223d
Alexander Makarov <sam@rmcreative.ru>
2022-02-01 23:02:32 +0300

    Adjust to RBAC changes

--- src/User/Console/CreateCommand.php
+++ src/User/Console/CreateCommand.php
@@ -6,7 +6,7 @@ namespace App\User\Console;
 
 use App\User\User;
 use App\User\UserRepository;
-use Exception;
+use InvalidArgumentException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,24 +14,20 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Throwable;
 use Yiisoft\Rbac\Manager;
-use Yiisoft\Rbac\RolesStorageInterface;
 use Yiisoft\Yii\Console\ExitCode;
 
 final class CreateCommand extends Command
 {
     private Manager $manager;
-    private RolesStorageInterface $rolesStorage;
     private UserRepository $userRepository;
 
     protected static $defaultName = 'user/create';
 
     public function __construct(
         Manager $manager,
-        RolesStorageInterface $rolesStorage,
         UserRepository $userRepository
     ) {
         $this->manager = $manager;
-        $this->rolesStorage = $rolesStorage;
         $this->userRepository = $userRepository;
         parent::__construct();
     }
@@ -59,18 +55,13 @@ final class CreateCommand extends Command
             $this->userRepository->save($user);
 
             if ($isAdmin) {
-                $role = $this->rolesStorage->getRoleByName('admin');
                 $userId = $user->getId();
 
-                if ($role === null) {
-                    throw new Exception('Role admin is NULL');
-                }
-
                 if ($userId === null) {
-                    throw new Exception('User Id is NULL');
+                    throw new InvalidArgumentException('User Id is NULL');
                 }
 
-                $this->manager->assign($role, $userId);
+                $this->manager->assign('admin', $userId);
             }
 
             $io->success('User created');
