0c9b50d64b0343f51949240f4d5f62c9fda5083f
Sergei Predvoditelev <sergei@predvoditelev.ru>
2022-05-17 16:54:29 +0300

    Adopt contacts form

--- src/Contact/views/contact/form.php
+++ src/Contact/views/contact/form.php
@@ -3,18 +3,15 @@
 declare(strict_types=1);
 
 use App\Widget\FlashMessage;
-use Yiisoft\Form\Widget\Field;
-use Yiisoft\Form\Widget\Form;
-use Yiisoft\Form\Widget\ResetButton;
-use Yiisoft\Form\Widget\SubmitButton;
+use Yiisoft\Form\Field;
 use Yiisoft\Html\Html;
+use Yiisoft\Html\Tag\Form;
 use Yiisoft\View\WebView;
 
 /**
  * @var Yiisoft\Yii\View\Csrf $csrf
  * @var \App\Contact\ContactForm $form
  * @var \Yiisoft\Router\UrlGeneratorInterface $url
- * @var \Yiisoft\Form\Widget\Field $field
  * @var WebView $this
  * @var \Yiisoft\Translator\TranslatorInterface $translator
  */
@@ -31,44 +28,43 @@ $this->setTitle($translator->translate('menu.contact'));
                 <div class="card-header bg-dark text-white">
                     <h1 class="fw-normal h3 text-center"><?= Html::encode($this->getTitle()) ?></h1>
                 </div>
-                    <div class="card-body p-5 text-center">
-                        <?= Form::widget()
-                            ->action($url->generate('site/contact'))
-                            ->csrf($csrf)
-                            ->id('form-contact')
-                            ->begin()
-                        ?>
+                <div class="card-body p-5 text-center">
+                    <?= Form::tag()
+                        ->post($url->generate('site/contact'))
+                        ->csrf($csrf)
+                        ->id('form-contact')
+                        ->open()
+                    ?>
 
-                            <?= Field::widget()->text($form, 'name') ?>
-                            <?= Field::widget()->email($form, 'email') ?>
-                            <?= Field::widget()->text($form, 'subject') ?>
-                            <?= Field::widget()->attributes(['style' => 'height: 100px'])->textArea($form, 'body') ?>
-                            <?= Field::widget()
-                                ->containerClass('mb-3')
-                                ->file($form, 'attachFiles', ['multiple()' => [true]])
-                                ->label(null)
-                            ?>
-                            <?= Field::widget()
-                                ->containerClass('btn-group btn-toolbar float-end')
-                                ->buttonGroup(
-                                    [
-                                        ['label' => 'Reset', 'type' => 'reset'],
-                                        ['label' => 'Submit', 'type' => 'submit'],
-                                    ],
-                                        ['individualButtonAttributes()' => [
-                                            [
-                                                0 => ['class' => 'btn btn-lg btn-danger'],
-                                                1 => ['class' => 'btn btn-lg btn-primary', 'name' => 'contact-button'],
-                                            ],
-                                        ],
-                                    ],
-                                )
-                            ?>
+                    <?= Field::text($form, 'name') ?>
+                    <?= Field::email($form, 'email') ?>
+                    <?= Field::text($form, 'subject') ?>
+                    <?= Field::textarea($form, 'body')->inputAttributes(['style' => 'height: 100px']) ?>
+                    <?= Field::file($form, 'attachFiles')
+                        ->replaceContainerClass('mb-3')
+                        ->multiple()
+                        ->hideLabel()
+                    ?>
+                    <?= Field::buttonGroup()
+                        ->containerClass('btn-group btn-toolbar float-end')
+                        ->buttonsData([
+                            [
+                                'Reset',
+                                'type' => 'reset',
+                                'class' => 'btn btn-lg btn-danger',
+                            ],
+                            [
+                                'Submit',
+                                'type' => 'submit',
+                                'class' => 'btn btn-lg btn-primary',
+                                'name' => 'contact-button',
+                            ],
+                        ]) ?>
 
-                        <?= Form::end() ?>
-                    </div>
+                    <?= Form::tag()->close() ?>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
