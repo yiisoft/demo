1571a8445b05174717fbb91ef55e48586a779a37
Sanka <aleksander@axendo.ee>
2022-06-17 20:11:45 +0300

    Fix #458: Clear db during testing (#461)

--- config/params.php
+++ config/params.php
@@ -130,6 +130,7 @@ return [
             'user/create' => App\User\Console\CreateCommand::class,
             'user/assignRole' => App\User\Console\AssignRoleCommand::class,
             'fixture/add' => App\Command\Fixture\AddCommand::class,
+            'fixture/schema/clear' => App\Command\Fixture\SchemaClearCommand::class,
             'router/list' => App\Command\Router\ListCommand::class,
             'translator/translate' => App\Command\Translation\TranslateCommand::class,
         ],
