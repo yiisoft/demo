9fa93b0e4e37ecd6b1235f821b2d827c07ae470c
Evgeniy Tkachenko <et.coder@gmail.com>
2022-07-19 20:59:52 +0300

    refactor: alias for FileCache

--- config/web/rate-limit.php
+++ config/web/rate-limit.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 use Yiisoft\Yii\RateLimiter\CounterInterface;
 use Yiisoft\Yii\RateLimiter\Storage\StorageInterface;
 use \Yiisoft\Yii\RateLimiter\Counter;
+use \Yiisoft\Cache\File\FileCache;
+use \Yiisoft\Aliases\Aliases;
+use \Yiisoft\Yii\RateLimiter\Storage\SimpleCacheStorage;
 
 /** @var array $params */
 
 return [
-    StorageInterface::class => function () {
-        $cache = new \Yiisoft\Cache\File\FileCache(__DIR__ . '/../runtime/rate-limit/');
-        return new \Yiisoft\Yii\RateLimiter\Storage\SimpleCacheStorage($cache);
+    StorageInterface::class => function (Aliases $aliases) {
+        $cache = new FileCache($aliases->get('@runtime/rate-limiter'));
+        return new SimpleCacheStorage($cache);
     },
     CounterInterface::class => [
         'class' => Counter::class,
