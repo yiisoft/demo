50ffbf7d812b2cc4ffe6df675edca7d7d05f0997
evan70 <evan70@users.noreply.github.com>
2022-08-09 23:46:57 +0200

    Add Slovak language >	modified:   resources/messages/en/app.php (#492)

--- resources/messages/en/app.php
+++ resources/messages/en/app.php
@@ -30,6 +30,7 @@ return [
     'layout.change-language' => 'Change language',
     'layout.language.english' => 'English',
     'layout.language.russian' => 'Русский',
+    'layout.language.slovak' => 'Slovak',
     'layout.language.indonesian' => 'Indonesia',
     'signup' => 'Signup',
     'layout.not-found' => 'Not found',
