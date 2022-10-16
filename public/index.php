d9e2f292bcc91ab41a025044af0822fe6ef350be
Wilmer Arambula <terabytesoftw@gmail.com>
2022-10-16 11:56:49 -0300

    Fix codeception tests, add codecov, add tests php `8.2`.

--- public/index.php
+++ public/index.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 use Yiisoft\Yii\Runner\Http\HttpApplicationRunner;
 
+if (getenv('YII_C3')) {
+    $c3 = dirname(__DIR__) . '/c3.php';
+    if (file_exists($c3)) {
+        require_once $c3;
+    }
+}
+
 /**
  * @psalm-var string $_SERVER['REQUEST_URI']
  */
-
 // PHP built-in server routing.
 if (PHP_SAPI === 'cli-server') {
     // Serve static files as is.
@@ -21,15 +27,7 @@ if (PHP_SAPI === 'cli-server') {
     $_SERVER['SCRIPT_NAME'] = '/index.php';
 }
 
-chdir(dirname(__DIR__));
 require_once dirname(__DIR__) . '/autoload.php';
-
-if (getenv('YII_ENV') === 'test') {
-    $c3 = 'c3.php';
-    if (file_exists($c3)) {
-        require_once $c3;
-    }
-}
 
 // Run HTTP application runner
 $runner = new HttpApplicationRunner(dirname(__DIR__), $_ENV['YII_DEBUG'], $_ENV['YII_ENV']);
