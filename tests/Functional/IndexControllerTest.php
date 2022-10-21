97067facea61816612b2a5e55e954dd3a8ebf604
Dmitrii Derepko <xepozz@list.ru>
2022-10-21 19:28:34 +0300

    Add functional tests

---
+++ tests/Functional/IndexControllerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use App\VersionProvider;
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Yii\Testing\FunctionalTester;
+
+final class IndexControllerTest extends TestCase
+{
+    private ?FunctionalTester $tester;
+
+    protected function setUp(): void
+    {
+        $this->tester = new FunctionalTester();
+    }
+
+    public function testGetIndex()
+    {
+        $method = 'GET';
+        $url = '/';
+
+        $this->tester->bootstrapApplication('web', dirname(__DIR__, 2));
+        $response = $this->tester->doRequest($method, $url);
+
+        $this->assertStringContainsString('Hello, everyone', $response->getContent());
+    }
+}
