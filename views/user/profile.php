7c2096275ef63628d215024922f790cba6e971d8
Wilmer Arambula <terabytesoftw@gmail.com>
2022-11-01 09:50:19 -0300

    Update last changes.

--- views/user/profile.php
+++ views/user/profile.php
@@ -9,8 +9,8 @@ declare(strict_types=1);
 
 use Yiisoft\Html\Html;
 use Yiisoft\Html\Tag\H2;
-use Yiisoft\Yii\DataView\Column\DetailColumn;
 use Yiisoft\Yii\DataView\DetailView;
+use Yiisoft\Yii\DataView\Field\DataField;
 
 $this->setTitle('Profile');
 
@@ -19,23 +19,23 @@ $title = Html::encode($this->getTitle());
 
 <?= DetailView::widget()
     ->attributes(['class' => 'container'])
-    ->columns(
-        DetailColumn::create()
+    ->containerAttributes(['class' => 'row flex-column justify-content-center align-items-center'])
+    ->data($item)
+    ->dataAttributes(['class' => 'col-xl-5'])
+    ->fields(
+        DataField::create()
             ->attribute('id')
             ->label('ID')
             ->value($item->getId()),
-        DetailColumn::create()
+        DataField::create()
             ->attribute('login')
             ->label($translator->translate('gridview.login'))
             ->value($item->getLogin()),
-        DetailColumn::create()
+        DataField::create()
             ->attribute('create_at')
             ->label($translator->translate('gridview.create.at'))
             ->value($item->getCreatedAt()->format('H:i:s d.m.Y')),
     )
-    ->containerAttributes(['class' => 'row flex-column justify-content-center align-items-center'])
-    ->data($item)
-    ->dataAttributes(['class' => 'col-xl-5'])
     ->header(H2::tag()->class('text-center')->content("<strong>$title</strong>")->encode(false)->render())
     ->labelAttributes(['class' => 'fw-bold'])
     ->valueAttributes(['class' => 'alert alert-info']);
