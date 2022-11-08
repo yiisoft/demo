7085b5ce1a0c68f55096565d4efc7400ea114d83
Rustam <rmamdaminov@gmail.com>
2022-11-08 21:19:42 +0500

    Adapt to translator changes

--- config/common/translator.php
+++ config/common/translator.php
@@ -4,18 +4,21 @@ declare(strict_types=1);
 
 use Yiisoft\Aliases\Aliases;
 use Yiisoft\Translator\CategorySource;
-use Yiisoft\Translator\Formatter\Intl\IntlMessageFormatter;
+use Yiisoft\Translator\IntlMessageFormatter;
 use Yiisoft\Translator\Message\Php\MessageSource;
 
 /** @var array $params */
 
 return [
     // Configure application CategorySource
-    'translation.app' => static function (Aliases $aliases) use ($params) {
-        return new CategorySource(
-            $params['yiisoft/translator']['defaultCategory'],
-            new MessageSource($aliases->get('@messages')),
-            new IntlMessageFormatter(),
-        );
-    },
+    'translation.app' => [
+        'definition' => static function (Aliases $aliases) use ($params) {
+            return new CategorySource(
+                $params['yiisoft/translator']['defaultCategory'],
+                new MessageSource($aliases->get('@messages')),
+                new IntlMessageFormatter(),
+            );
+        },
+        'tags' => ['translation.categorySource']
+    ],
 ];
