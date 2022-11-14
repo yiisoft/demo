dc0cb45dd326bc8d6039244472669f02b889065a
Dmitrii Derepko <xepozz@list.ru>
2022-11-14 20:14:33 +0300

    Clone changes

--- blog-api/config/params.php
+++ blog-api/config/params.php
@@ -59,11 +59,6 @@ return [
         'locale'          => 'en',
         'fallbackLocale'  => 'en',
         'defaultCategory' => 'app',
-        'categorySources' => [
-            // You can add categories from your application and additional modules using `Reference::to` below
-            // Reference::to(ApplicationCategorySource::class),
-            Reference::to('translation.app'),
-        ],
     ],
 
     // Console commands
