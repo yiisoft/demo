929a569178a2db435116edaba1cf1fc28742f809
Dmitrii Derepko <xepozz@list.ru>
2023-03-28 00:28:41 +0300

    Add an example how to deal with route attributes

--- blog/src/Controller/SiteController.php
+++ blog/src/Controller/SiteController.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Middleware\ApiDataWrapper;
 use Psr\Http\Message\ResponseInterface;
+use Yiisoft\DataResponse\Middleware\FormatDataResponseAsJson;
+use Yiisoft\DataResponse\Middleware\FormatDataResponseAsXml;
 use Yiisoft\Yii\View\ViewRenderer;
+use Yiisoft\Router\Route;
 
 final class SiteController
 {
@@ -14,7 +18,40 @@ final class SiteController
         $this->viewRenderer = $viewRenderer->withController($this);
     }
 
+    #[Route(
+        methods: ['GET'],
+        pattern: '/',
+        name: 'site/index',
+    )]
     public function index(): ResponseInterface
+    {
+        return $this->viewRenderer->render('index');
+    }
+
+    #[Route(
+        methods: ['GET'],
+        pattern: '/json',
+        name: 'site/index.json',
+        middlewares: [
+            FormatDataResponseAsJson::class,
+            ApiDataWrapper::class,
+        ],
+    )]
+    public function json(): ResponseInterface
+    {
+        return $this->viewRenderer->render('index');
+    }
+
+    #[Route(
+        methods: ['GET'],
+        pattern: '/xml',
+        name: 'site/index.xml',
+        middlewares: [
+            FormatDataResponseAsXml::class,
+            ApiDataWrapper::class,
+        ],
+    )]
+    public function xml(): ResponseInterface
     {
         return $this->viewRenderer->render('index');
     }
