8ee020bbc2951e0529c423ea4a09d1a6b62b9237
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-04-25 18:16:53 +0300

    Improve config

--- blog/config/web/di/middleware-dispatcher.php
+++ blog/config/web/di/middleware-dispatcher.php
@@ -9,17 +9,16 @@ declare(strict_types=1);
 
 use Vjik\InputHttp\ParametersResolver\InputAttributeParametersResolver;
 use Vjik\InputHttp\ParametersResolver\RequestModelParametersResolver;
-use Yiisoft\Definitions\DynamicReference;
 use Yiisoft\Definitions\Reference;
 use Yiisoft\Middleware\Dispatcher\CompositeParametersResolver;
 use Yiisoft\Middleware\Dispatcher\ParametersResolverInterface;
 
 return [
-    ParametersResolverInterface::class => DynamicReference::to([
+    ParametersResolverInterface::class => [
         'class' => CompositeParametersResolver::class,
         '__construct()' => [
             Reference::to(InputAttributeParametersResolver::class),
             Reference::to(RequestModelParametersResolver::class),
         ],
-    ]),
+    ],
 ];
