4ef916b36f40009f11debe735b184b2759f6bee7
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-05-28 19:36:33 +0300

    Set default value for "_language" in URL generator

--- blog-api/config/common/di/router.php
+++ blog-api/config/common/di/router.php
@@ -6,12 +6,17 @@ use App\Middleware\ExceptionMiddleware;
 use Yiisoft\Config\Config;
 use Yiisoft\DataResponse\Middleware\FormatDataResponse;
 use Yiisoft\Request\Body\RequestBodyParser;
+use Yiisoft\Router\FastRoute\UrlGenerator;
 use Yiisoft\Router\Group;
 use Yiisoft\Router\RouteCollection;
 use Yiisoft\Router\RouteCollectionInterface;
 use Yiisoft\Router\RouteCollectorInterface;
+use Yiisoft\Router\UrlGeneratorInterface;
 
-/** @var Config $config */
+/**
+ * @var Config $config
+ * @var array $params
+ */
 
 return [
     RouteCollectionInterface::class => static function (RouteCollectorInterface $collector) use ($config) {
@@ -21,9 +26,17 @@ return [
             ->middleware(RequestBodyParser::class)
             ->addGroup(
                 Group::create('/{_language}')
-                    ->routes(...$config->get('routes'))
+                     ->routes(...$config->get('routes'))
             );
 
         return new RouteCollection($collector);
     },
+    UrlGeneratorInterface::class => [
+        'class' => UrlGenerator::class,
+        'setEncodeRaw()' => [$params['yiisoft/router-fastroute']['encodeRaw']],
+        'setDefaultArgument()' => ['_language', 'en'],
+        'reset' => function () {
+            $this->defaultArguments = [];
+        },
+    ],
 ];
