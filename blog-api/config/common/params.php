6dbcb105bb495b764413c19ea17bfd6e6136acdb
Dmitriy Derepko <xepozz@list.ru>
2024-05-29 02:29:46 +0300

    Ignore locale middleware for gii controllers (#618)

--- blog-api/config/common/params.php
+++ blog-api/config/common/params.php
@@ -20,6 +20,7 @@ return [
     'locale' => [
         'locales' => ['en' => 'en-US', 'ru' => 'ru-RU', 'de' => 'de-DE'],
         'ignoredRequests' => [
+            '/gii**',
             '/debug**',
             '/inspect**',
         ],
